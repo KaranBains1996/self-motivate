f49aa131a1f7375c5d1ca2953401ccca539e88cc
karan.b <karan.b@axiomio.com>
2019-10-22 10:43:22 +0530

    Fix overflow scroll

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,54 +1,9 @@
 import { trigger, transition, query, style, animateChild, group, animate } from '@angular/animations';
 
-export const gateAnimation =
-  trigger('routeAnimations', [
-    transition('* <=> *', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          width: '100%',
-          height: '100%'
-        })
-      ], { optional: true }),
-      query(':enter', [
-        style({
-          opacity: 0,
-        }),
-      ], { optional: true }),
-      group([
-        query('.transition-top', [
-          animate('0.5s ease-in-out', style({ transform: 'translateY(0%)' }))
-        ]),
-        query('.transition-bottom', [
-          animate('0.5s ease-in-out', style({ transform: 'translateY(0%)' }))
-        ]),
-      ]),
-      query(':leave', [
-        style({
-          opacity: 0,
-        }),
-      ], { optional: true }),
-      query(':enter', [
-        style({
-          opacity: 1,
-        }),
-      ], { optional: true }),
-      group([
-        query('.transition-top', [
-          animate('0.5s ease-in-out', style({ transform: 'translateY(-102%)' }))
-        ]),
-        query('.transition-bottom', [
-          animate('0.5s ease-in-out', style({ transform: 'translateY(100%)' }))
-        ]),
-      ]),
-    ])
-  ]);
-
 export const slideExpandAnimation =
   trigger('routeAnimations', [
     transition('* <=> *', [
-      style({ position: 'relative' }),
+      style({position: 'relative' }),
       query(':leave, :enter', [
         style({
           position: 'absolute',
@@ -75,7 +30,7 @@ export const slideExpandAnimation =
       query(':enter', [
         animate('0.5s', style({
           height: '100%',
-          top: 0
+          top: 0,
         }))
       ], { optional: true }),
       query('.content', [
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { gateAnimation, slideExpandAnimation } from './animations';
+import { slideExpandAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
-    // gateAnimation,
     slideExpandAnimation
   ]
 })
